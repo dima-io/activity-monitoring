6da79df9baec2e7d6595c58935ca5b6a3df7cb3d
Dmytro Kondratenko <kondratenkodima364@gmail.com>
2026-06-01 15:47:11 +0300

    fix cors for vercel

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,15 @@ import activityRoutes from "./routes/activity.routes.js";
 import notificationRoutes from "./routes/notification.routes.js";
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:8080",
+      "https://activity-monitoring-blond.vercel.app",
+    ],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
