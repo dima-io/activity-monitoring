f030172b021498402380cbc4c5b6131608497ec0
Dmytro Kondratenko <kondratenkodima364@gmail.com>
2026-06-02 23:34:36 +0300

    Update PWA icons

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,16 @@ const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
   transpileDependencies: ["vuetify"],
+
+  pwa: {
+    name: "Activity Monitoring",
+
+    themeColor: "#1976D2",
+
+    msTileColor: "#1976D2",
+
+    appleMobileWebAppCapable: "yes",
+
+    appleMobileWebAppStatusBarStyle: "black",
+  },
 });
